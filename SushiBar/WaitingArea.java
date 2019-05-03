d0f1ac5c46831188baa154e4fc25439d6b489740
Helle van den Broek <hvbroek@stud.ntnu.no>
2019-05-03 10:48:55 +0200

    Added local project

---
+++ SushiBar/WaitingArea.java
@@ -0,0 +1,35 @@
+import java.util.LinkedList;
+
+/**
+ * This class implements a waiting area used as the bounded buffer, in the producer/consumer problem.
+ */
+public class WaitingArea {
+
+    /**
+     * Creates a new waiting area.
+     * areaSize decides how many people can be waiting at the same time (how large the shared buffer is)
+     *
+     * @param size The maximum number of Customers that can be waiting.
+     */
+    public WaitingArea(int size) {
+        // TODO Implement required functionality
+    }
+
+    /**
+     * This method should put the customer into the waitingArea
+     *
+     * @param customer A customer created by Door, trying to enter the waiting area
+     */
+    public synchronized void enter(Customer customer) {
+        // TODO Implement required functionality
+    }
+
+    /**
+     * @return The customer that is first in line.
+     */
+    public synchronized Customer next() {
+        // TODO Implement required functionality
+    }
+
+    // Add more methods as you see fit
+}
